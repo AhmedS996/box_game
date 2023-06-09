e00b3bd67c8ba5e3218b622dce0d508f8e8b4913
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2023-06-09 07:47:17 +0300

    edit code with comments

--- JS/levelOne.js
+++ JS/levelOne.js
@@ -1,18 +1,25 @@
+// Generate random number to determine which box contains the bomb
 var bombBox = Math.floor(Math.random() * 9) + 1;
 
+// Initialize count variable to keep track of how many boxes have been clicked
 var count = 0;
+
+// Function to handle box clicks
 function handleBoxClick(boxNumber) {
+  // Get the box element that was clicked
   var box = document.getElementById("box" + boxNumber);
 
+  // If the clicked box contains the bomb, show the lose modal and highlight the box in red
   if (boxNumber === bombBox) {
     box.classList.add("red");
-
     showModal();
-  } else {
+  }
+  // If the clicked box does not contain the bomb, highlight the box in green and disable clicking on it
+  else {
     box.classList.add("green");
-
     box.onclick = null; // disable further clicking
     count++;
+    // If all boxes except the bomb have been clicked, show the win modal
     var modal_s = document.getElementById("myModal_s");
     if (count == 8) {
       modal_s.style.display = "block";
@@ -20,31 +27,33 @@ function handleBoxClick(boxNumber) {
   }
 }
 
+// Function to show the lose modal
 function showModal() {
-  // Get the modal
+  // Get the lose modal element
   var modal = document.getElementById("myModal");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
+  // Show the lose modal
   modal.style.display = "block";
-  // When the user clicks on <span> (x), close the modal
+
+  // When the user clicks on <span> (x), close the modal and disable clicking on boxes
   span.onclick = function () {
     modal.style.display = "none";
     disableBoxClicking();
   };
 
-  // When the user clicks anywhere outside of the modal, close it
+  // When the user clicks anywhere outside of the modal, close it and disable clicking on boxes
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
       disableBoxClicking();
     }
   };
-
-  // Show the modal  modal.style.display = 'block';
 }
 
+// Function to disable clicking on boxes
 function disableBoxClicking() {
   // Loop through all boxes and disable clicking on them
   var boxes = document.getElementsByClassName("box");
--- JS/levelTwo.js
+++ JS/levelTwo.js
@@ -1,21 +1,28 @@
+// Generate two random numbers to determine which boxes contain the bombs
 var bombBoxes = [
   Math.floor(Math.random() * 9) + 1,
   Math.floor(Math.random() * 9) + 1,
 ];
+
+// Initialize count variable to keep track of how many boxes have been clicked
 var count = 0;
 
+// Function to handle box clicks
 function handleBoxClick(boxNumber) {
+  // Get the box element that was clicked
   var box = document.getElementById("box" + boxNumber);
 
+  // If the clicked box contains a bomb, show the lose modal and highlight the box in red
   if (bombBoxes.includes(boxNumber)) {
     box.classList.add("red");
-
     showModal();
-  } else {
+  }
+  // If the clicked box does not contain a bomb, highlight the box in green and disable clicking on it
+  else {
     box.classList.add("green");
-
     box.onclick = null; // disable further clicking
     count++;
+    // If all boxes except the bombs have been clicked, show the win modal
     var modal_s = document.getElementById("myModal_s");
     if (count == 7) {
       modal_s.style.display = "block";
@@ -23,31 +30,33 @@ function handleBoxClick(boxNumber) {
   }
 }
 
+// Function to show the lose modal
 function showModal() {
-  // Get the modal
+  // Get the lose modal element
   var modal = document.getElementById("myModal");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
+  // Show the lose modal
   modal.style.display = "block";
-  // When the user clicks on <span> (x), close the modal
+
+  // When the user clicks on <span> (x), close the modal and disable clicking on boxes
   span.onclick = function () {
     modal.style.display = "none";
     disableBoxClicking();
   };
 
-  // When the user clicks anywhere outside of the modal, close it
+  // When the user clicks anywhere outside of the modal, close it and disable clicking on boxes
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
       disableBoxClicking();
     }
   };
-
-  // Show the modal  modal.style.display = 'block';
 }
 
+// Function to disable clicking on boxes
 function disableBoxClicking() {
   // Loop through all boxes and disable clicking on them
   var boxes = document.getElementsByClassName("box");
